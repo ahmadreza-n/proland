4f710b6f9f506d5a0f1692038737f3c3213f7549
Ahmadreza-n <ahmadrezaanazari@gmail.com>
2020-01-30 15:48:23 +0330

    nothing

--- src/HomePage/CardList.js
+++ src/HomePage/CardList.js
@@ -16,7 +16,7 @@ class Example extends PureComponent {
                     <h5 className="card-title">{l.name}</h5>
                     <p className="card-text">
                       {`Some quick example text to build on the card title
-               and make up the bulk of the card's content.`}
+                        and make up the bulk of the card's content.`}
                     </p>
                     <a href="#" className="btn btn-primary">Go somewhere</a>
                   </div>
